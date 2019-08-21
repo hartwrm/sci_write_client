80fa3f21fd9534a156019ade63cd94021a7964b6
Andrew Hartzell <hartwrm@Macs-MacBook-Pro.local>
2019-08-21 19:33:57 -0400

    render text editor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import ReactDOM from 'react-dom';
+import {Editor, EditorState} from 'draft-js';
 import WritingPrompt from './components/WritingPrompt'
+import MyEditor from './components/MyEditor'
 
 class App extends Component{
+
   render(){
     return(
       <div className="App">
-        <h1>Hello World</h1>
         <WritingPrompt />
+        <MyEditor />
       </div>
     )
   }
